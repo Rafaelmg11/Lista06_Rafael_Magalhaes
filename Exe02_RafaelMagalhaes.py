a2f17d6b3587a74a690404221f796c28b6b4c0a8
Rafaelmg11 <almeidarafaelmagalhaes@gmail.com>
2025-02-21 19:52:58 -0300

    up

--- Exe02_RafaelMagalhaes.py
+++ Exe02_RafaelMagalhaes.py
@@ -6,12 +6,29 @@
 
 produtosLista = []
 
-for i in range(10):
-    produtosLista.append(input("Insira um produto: "))
+
+for i in range(4):
+    produto = (input("Insira um produto: "))
+
+    produto = produto.lower()
+
+    produtosLista.append(produto)
+    indiceLista = produtosLista.index(produto)
+    print("O indice desse de {} é : {}".format(produto,indiceLista))
 
 #DECLARANDO UMA DUPLA ADICIONANDO A LISTA NA TUPLA
 produtosTupla = tuple(produtosLista)
 
 print("Produtos: ",produtosTupla)
 
+indiceTupla = (input("Digite um número de 0 á 9: "))
 
+try:
+    print("Produto:{}".format(produtosTupla(indiceTupla)))
+
+except:
+    while indiceTupla not in produtosTupla(indiceTupla):
+        print("Esse produto não existe!")
+        print("Tente Novamete!\n")
+        indiceTupla = (input("Digite um número de 0 á 9: "))
+    print("Produto:{}".format(produtosTupla(indiceTupla)))
